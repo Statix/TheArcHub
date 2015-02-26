e81f16b84bde40a81772790df77aac3e77baaaac
Statix <corleykidz@gmail.com>
2015-02-26 14:24:37 -0600

    Update Main.java

--- src/net/thearc/hub/main/Main.java
+++ src/net/thearc/hub/main/Main.java
@@ -16,5 +16,12 @@ public class Main extends JavaPlugin {
 		version = 0.1;
 		rights = "Created by ItzQuiver, owned by TheArc.";
 	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		//You dont need to worry about join or leave messages. Duh already told me what to make with some other 
+		//stuff. Such as /welcome ect... ~Matt
+	}
 
 }
